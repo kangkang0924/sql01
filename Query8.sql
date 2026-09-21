7987197a34b8b7fd5290bcded61a11cacd10306e
kangkang <1790516800@qq.com>
2026-09-21 11:12:36 +0800

    sql优化count

--- Query8.sql
+++ Query8.sql
@@ -7,6 +7,8 @@ drop index idx_user_name on tb_user;
 drop index idx_user_pro_age_sta on tb_user;
 drop index idx_email_5 on tb_user;
 
+create index idx_user_pro_age_sta on tb_user(profession,age,status);
+
 select profession , count(*) from tb_user group by profession ;
 
 select profession ,age, count(*) from tb_user group by profession,age ;
