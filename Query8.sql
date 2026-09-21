984e002ed68e923c18676e0a90a2af2ef56c57b9
kangkang <1790516800@qq.com>
2026-09-21 10:15:25 +0800

    sql优化

---
+++ Query8.sql
@@ -0,0 +1,16 @@
+show index from tb_user;
+
+drop index idx_user_phone on tb_user;
+drop index idx_user_phone_name on tb_user;
+drop index idx_user_name on tb_user;
+
+drop index idx_user_pro_age_sta on tb_user;
+drop index idx_email_5 on tb_user;
+
+select profession , count(*) from tb_user group by profession ;
+
+select profession ,age, count(*) from tb_user group by profession,age ;
+
+explain select profession , count(*) from tb_user group by profession ;
+
+explain select profession ,age, count(*) from tb_user group by profession,age ;
