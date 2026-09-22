83d1f008b7752f96f59b1a7bad4c94bc21f82754
kangkang <1790516800@qq.com>
2026-09-22 14:28:29 +0800

    存储过程

--- 存储过程.sql
+++ 存储过程.sql
@@ -45,3 +45,214 @@ begin
     select stu_count;
 end;
 call p2();
+
+create procedure p3()
+begin
+    declare score int default 58;
+    declare result varchar(10);
+    if score >= 85 then
+        set result := '优秀';
+    elseif score >= 60 then
+        set result := '及格';
+    else
+        set result := '不及格';
+    end if;
+    select result;
+end;
+call p3();
+
+create procedure p4(in score int, out result varchar(10))
+begin
+    if score >= 85 then
+        set result := '优秀';
+    elseif score >= 60 then
+        set result := '及格';
+    else
+        set result := '不及格';
+    end if;
+end;
+-- 定义用户变量 @result来接收返回的数据, 用户变量可以不用声明
+call p4(18, @result);
+select @result;
+
+
+create procedure p5(inout score double)
+begin
+    set score := score * 0.5;
+end;
+set @score = 198;
+call p5(@score);
+select @score;
+
+create procedure p6(in month int)
+begin
+    declare result varchar(10);
+    case
+        when month >= 1 and month <= 3 then
+            set result := '第一季度';
+        when month >= 4 and month <= 6 then
+            set result := '第二季度';
+        when month >= 7 and month <= 9 then
+            set result := '第三季度';
+        when month >= 10 and month <= 12 then
+            set result := '第四季度';
+        else
+            set result := '非法参数';
+        end case ;
+    select concat('您输入的月份为: ',month, ', 所属的季度为: ',result);
+end;
+call p6(4);
+
+
+
+-- 计算从1累加到n的值，n为传入的参数值。
+-- A. 定义局部变量, 记录累加之后的值;
+-- B. 每循环一次, 就会对n进行减1 , 如果n减到0, 则退出循环
+create procedure p7(in n int)
+begin
+    declare total int default 0;
+    while n>0 do
+            set total := total + n;
+            set n := n - 1;
+        end while;
+    select total;
+end;
+call p7(100);
+
+set @score1 = 198 + 1;
+
+
+-- A. 定义局部变量, 记录累加之后的值;
+-- B. 每循环一次, 就会对n进行-1 , 如果n减到0, 则退出循环
+create procedure p8(in n int)
+begin
+    declare total int default 0;
+    repeat
+        set total := total + n;
+        set n := n - 1;
+    until n <= 0
+        end repeat;
+    select total;
+end;
+call p8(10);
+call p8(100);
+
+-- A. 定义局部变量, 记录累加之后的值;
+-- B. 每循环一次, 就会对n进行-1 , 如果n减到0, 则退出循环 ----> leave xx
+create procedure p9(in n int)
+begin
+    declare total int default 0;
+    sum:loop
+        if n<=0 then
+            leave sum;
+        end if;
+        set total := total + n;
+        set n := n - 1;
+    end loop sum;
+    select total;
+end;
+call p9(100);
+
+
+-- A. 定义局部变量, 记录累加之后的值;
+-- B. 每循环一次, 就会对n进行-1 , 如果n减到0, 则退出循环 ----> leave xx
+-- C. 如果当次累加的数据是奇数, 则直接进入下一次循环. --------> iterate xx
+create procedure p10(in n int)
+begin
+    declare total int default 0;
+    sum:loop
+        if n<=0 then
+            leave sum;
+        end if;
+        if n%2 = 1 then
+            set n := n - 1;
+            iterate sum;
+        end if;
+        set total := total + n;
+        set n := n - 1;
+    end loop sum;
+    select total;
+end;
+call p10(100);
+
+
+-- 逻辑:
+-- A. 声明游标, 存储查询结果集
+-- B. 准备: 创建表结构
+-- C. 开启游标
+-- D. 获取游标中的记录
+-- E. 插入数据到新表中
+-- F. 关闭游标
+create procedure p11(in uage int)
+begin
+    declare uname varchar(100);
+    declare upro varchar(100);
+    declare u_cursor cursor for select name,profession from tb_user where age <=
+                                                                          uage;
+    drop table if exists tb_user_pro;
+    create table if not exists tb_user_pro(
+                                              id int primary key auto_increment,
+                                              name varchar(100),
+                                              profession varchar(100)
+    );
+    open u_cursor;
+    while true do
+            fetch u_cursor into uname,upro;
+            insert into tb_user_pro values (null, uname, upro);
+        end while;
+    close u_cursor;
+end;
+call p11(30);
+
+-- 逻辑:
+-- A. 声明游标, 存储查询结果集
+-- B. 准备: 创建表结构
+-- C. 开启游标
+-- D. 获取游标中的记录
+-- E. 插入数据到新表中
+-- F. 关闭游标
+create procedure p11(in uage int)
+begin
+    declare uname varchar(100);
+    declare upro varchar(100);
+    declare u_cursor cursor for select name,profession from tb_user where age <=
+                                                                          uage;
+    -- 声明条件处理程序 ： 当SQL语句执行抛出的状态码为02000时，将关闭游标u_cursor，并退出
+    declare exit handler for SQLSTATE '02000' close u_cursor;
+    drop table if exists tb_user_pro;
+    create table if not exists tb_user_pro(
+                                              id int primary key auto_increment,
+                                              name varchar(100),
+                                              profession varchar(100)
+    );
+    open u_cursor;
+    while true do
+            fetch u_cursor into uname,upro;
+            insert into tb_user_pro values (null, uname, upro);
+        end while;
+    close u_cursor;
+end;
+call p11(30);
+
+
+create procedure p12(in uage int)
+begin
+    declare uname varchar(100);
+    declare upro varchar(100);
+    declare u_cursor cursor for select name,profession from tb_user where age <=
+                                                                          uage;
+-- 声明条件处理程序 ： 当SQL语句执行抛出的状态码为02开头时，将关闭游标u_cursor，并退出
+    declare exit handler for not found close u_cursor;
+    drop table if exists tb_user_pro;
+    create table if not exists tb_user_pro(
+                                              id int primary key auto_increment,name varchar(100),
+                                              profession varchar(100)
+    );
+    open u_cursor;
+    while true do
+            fetch u_cursor into uname,upro;
+            insert into tb_user_pro values (null, uname, upro);
+        end while;
+    close u_cursor;
+end;
+call p12(30);
